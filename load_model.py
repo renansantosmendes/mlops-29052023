3f7f8df7655bf60435518fa346ad7a266d3845ff
Renan Santos Mendes <renansantosmendes@gmail.com>
2023-07-09 11:03:53 -0300

    upload files

--- load_model.py
+++ load_model.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def config_mlflow():
-    MLFLOW_TRACKING_URI = 'https://dagshub.com/renansantosmendes/teste.mlflow'
+    MLFLOW_TRACKING_URI = 'https://dagshub.com/renansantosmendes/mlops-puc-repository.mlflow'
     MLFLOW_TRACKING_USERNAME = 'renansantosmendes'
     MLFLOW_TRACKING_PASSWORD = '6d730ef4a90b1caf28fbb01e5748f0874fda6077'
     os.environ['MLFLOW_TRACKING_USERNAME'] = MLFLOW_TRACKING_USERNAME
@@ -16,15 +16,17 @@ def config_mlflow():
     return client
 
 
-def load_model(client):
+def load_model_from_registry(client):
+    mlflow.set_tracking_uri('https://dagshub.com/renansantosmendes/mlops-puc-repository.mlflow')
+    client = mlflow.MlflowClient(tracking_uri='https://dagshub.com/renansantosmendes/mlops-puc-repository.mlflow')
     registered_model = client.get_registered_model('fetal_health_model_v1')
     run_id = registered_model.latest_versions[-1].run_id
     logged_model = f'runs:/{run_id}/model'
-    loaded_model = mlflow.pyfunc.load_model_from_registry(logged_model)
+    loaded_model = mlflow.pyfunc.load_model(logged_model)
     return loaded_model
 
 
 if __name__ == "__main__":
-    model = load_model(config_mlflow())
+    model = load_model_from_registry(config_mlflow())
     prediction = model.predict(data=np.array([[-0.5, -0.5, -0.5, -0.5]]))
     print(prediction)
